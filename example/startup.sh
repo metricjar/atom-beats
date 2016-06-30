22d214fd2a5d2c8fbeb90550bf88b130f2842de7
zeevmanilovich <zeev.manilovich@ironsrc.com>
2016-06-30 15:34:38 +0300

    fix(*): removing filebeat binary and adding bash shebang

--- example/startup.sh
+++ example/startup.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+# note: make sure you have logstash and filebeat installed and in added to PATH env var.
 STREAM=$STREAM AUTH=$AUTH logstash --allow-env -f logstash.conf &
 `sleep 15` ./filebeat -e -d "*" -c filebeat.yml
